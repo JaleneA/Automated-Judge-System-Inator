28b811d515a80fd6f6c90906154466d971fb6653
Rikuthin <125291285+Zurun-Rikuthin@users.noreply.github.com>
2024-11-17 11:37:04 -0400

    Docstrings for App.java and StudentService.java

--- src/main/java/servicelocator/StudentService.java
+++ src/main/java/servicelocator/StudentService.java
@@ -1,19 +1,37 @@
-/**
- * @author jalenearmstrong
- * Helpful Documentation
- * https://www.baeldung.com/java-service-locator-pattern
- */
-
 package servicelocator;
 
+/**
+ * The StudentService class
+ *
+ * @author jalenearmstrong
+ * @see <a href="https://www.baeldung.com/java-service-locator-pattern">Service
+ * Locator Pattern and Java Implementation</a>
+ */
 public class StudentService {
-    private static String currentStudentName;
- 
-    public static void setCurrentStudentName(String studentName) {
-        currentStudentName = studentName;
-    }
 
+    // -- INSTANCE VARIABLES
+    /**
+     *The current student's name.
+     */
+    private static String currentStudentName;
+
+    // -- GETTERS --
+    /**
+     * Returns the current student's name.
+     *
+     * @return The current student's name.
+     */
     public static String getCurrentStudentName() {
         return currentStudentName;
+    }
+
+    // -- SETTERS --
+    /**
+     * Sets the current student's name.
+     *
+     * @param studentName The current student's name.
+     */
+    public static void setCurrentStudentName(String studentName) {
+        currentStudentName = studentName;
     }
 }
